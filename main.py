39d4965ad3da997e7791562c16f249a4385cc08a
nekrasov445 <maksimnekrasov449@gmail.com>
2021-01-01 16:53:11 +0700

    default with start screen

--- main.py
+++ main.py
@@ -27,10 +27,35 @@ def terminate():
 # класс начальной заставки
 class StartScreen:
     def __init__(self):
-        pass
+        self.fon = pygame.transform.scale(load_image('Battle.jpg'), (width, height))
+        self.clock = pygame.time.Clock()
 
     def draw(self):
-        pass
+        intro_text = ["Игра", "",  # пока написал так, потом можно поправить
+                      "Морской бой",
+                      "классический"]
+
+        screen.blit(self.fon, (0, 0))
+        font = pygame.font.Font(None, 30)
+        text_coord = 50
+        for line in intro_text:
+            string_rendered = font.render(line, 1, pygame.Color('black'))
+            intro_rect = string_rendered.get_rect()
+            text_coord += 10
+            intro_rect.top = text_coord
+            intro_rect.x = 10
+            text_coord += intro_rect.height
+            screen.blit(string_rendered, intro_rect)
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    terminate()
+                elif event.type == pygame.KEYDOWN or \
+                        event.type == pygame.MOUSEBUTTONDOWN:
+                    return  # начинаем игру
+            pygame.display.flip()
+            self.clock.tick(FPS)
 
 
 class SavesScreen:
@@ -97,7 +122,11 @@ pygame.init()
 size = width, height = 600, 400
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption('Морской бой')
+
 all_sprites = pygame.sprite.Group()
+start_screen = StartScreen()
+start_screen.draw()
+
 clock = pygame.time.Clock()
 # таймер использовать будем при ходе ИИ
 TIMER = pygame.USEREVENT + 1
@@ -106,4 +135,6 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             terminate()
+    screen.fill(pygame.Color('white'))
+    pygame.display.flip()
     clock.tick(FPS)
